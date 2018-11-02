76c57c22cf7b85a7eae73ef67be94f871ded107c
Jack Dibden <s5083071@bournemouth.ac.uk>
2018-11-02 13:48:22 +0000

    Completed level

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -15,31 +15,41 @@ public class PlayerController : MonoBehaviour {
 
 	RaycastHit hit;
 
-	// Use this for initialization
-	void Start () {
+	public bool isMoving = false;
+
+	void Start () 
+	{
 		myAnim = GetComponent<Animator> ();
 	}
-	
-	// Update is called once per frame
-	void Update () {
-		if(Input.GetMouseButtonDown(0)) {
+
+	void Update () 
+	{
+		if(Input.GetMouseButtonDown(0)) 
+		{
 			Ray ray = camera.ScreenPointToRay (Input.mousePosition);
-			if (Physics.Raycast (ray, out hit)) {
+			if (Physics.Raycast (ray, out hit)) 
+			{
 				agent.SetDestination (hit.point);
 				myAnim.SetBool ("isRunning", true);
+				isMoving = true;
 			}
 		}
 
-		Vector3 relativePos = hit.point - transform.position;
-		newRotation = Quaternion.LookRotation (relativePos, Vector3.up);
-		newRotation.x = 0.0f;
-		newRotation.z = 0.0f;
+		if (isMoving == true) 
+		{
+			Vector3 relativePos = hit.point - transform.position;
+			newRotation = Quaternion.LookRotation (relativePos, Vector3.up);
+			newRotation.x = 0.0f;
+			newRotation.z = 0.0f;
 
-		transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotSpeed * Time.deltaTime);
+			transform.rotation = Quaternion.Slerp(transform.rotation, newRotation, rotSpeed * Time.deltaTime);
 
-		dist = Vector3.Distance (hit.point, transform.position);
-		if (dist < 0.5f) {
-			myAnim.SetBool ("isRunning", false);
+			dist = Vector3.Distance (hit.point, transform.position);
+			if (dist < 0.5f) 
+			{
+				myAnim.SetBool ("isRunning", false);
+				isMoving = false;
+			}
 		}
 	}
 }
